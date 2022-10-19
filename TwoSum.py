719265076254c05545f957429b65416514fe303e
Nuttanan29445 <nuch29445@gmail.com>
2022-10-19 14:43:14 +0700

    TwoSum

---
+++ TwoSum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        z = []
+        for j,i in enumerate(nums):
+            v = j
+            for k in nums[j+1:len(nums)]:
+                v+=1
+                if int(i)+int(k) == target:
+                    return[j,v]
+        
+                
+
+                
+
+                    
+
