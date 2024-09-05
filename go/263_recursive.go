abd9b07dc753e5a0504cd638ed843030ccc488f2
Nuttanan29445 <nuch29445@gmail.com>
2024-09-05 16:32:47 +0700

    go_263

---
+++ go/263_recursive.go
@@ -0,0 +1,21 @@
+package leetcode
+
+func isUgly(n int) bool {
+	for {
+		if n == 0 {
+			return false
+		}
+		if n == 1 {
+			return true
+		}
+
+		if n%2 == 0 {
+			return isUgly(n / 2)
+		} else if n%3 == 0 {
+			return isUgly(n / 3)
+		} else if n%5 == 0 {
+			return isUgly(n / 5)
+		}
+		return false
+	}
+}
