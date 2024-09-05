061ee4a4c9f627ff27c4e1d772c759bf4c3ef010
Nuttanan29445 <nuch29445@gmail.com>
2024-09-05 18:07:40 +0700

    mergeSol

--- go/228.go
+++ go/228.go
@@ -36,3 +36,25 @@ func summaryRanges(nums []int) []string {
 
 	return ans
 }
+
+func summaryRanges2(nums []int) []string {
+    var ans []string
+    i := 0
+    for i < len(nums) {
+        start := i
+        
+        for i+1 < len(nums) && nums[i+1] == nums[i]+1 {
+            i++
+        }
+  
+        if start == i {
+           
+            ans = append(ans, fmt.Sprintf("%v", nums[start]))
+        } else {
+           
+            ans = append(ans, fmt.Sprintf("%v->%v", nums[start], nums[i]))
+        }
+        i++
+    }
+    return ans
+}
--- go/263.go
+++ go/263.go
@@ -20,3 +20,23 @@ func isUgly(n int) bool {
 		}
 	}
 }
+
+func isUglyRecursive(n int) bool {
+	for {
+		if n == 0 {
+			return false
+		}
+		if n == 1 {
+			return true
+		}
+
+		if n%2 == 0 {
+			return isUgly(n / 2)
+		} else if n%3 == 0 {
+			return isUgly(n / 3)
+		} else if n%5 == 0 {
+			return isUgly(n / 5)
+		}
+		return false
+	}
+}
