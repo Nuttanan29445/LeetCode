7dd75a9199314b2ea5b3f03668482dbb06a315d5
Nuttanan29445 <nuch29445@gmail.com>
2024-09-06 15:30:21 +0700

    go_342_O1Sol

--- go/342.go
+++ go/342.go
@@ -12,3 +12,8 @@ func isPowerOfFour(n int) bool {
 	}
 	return false
 }
+
+func isPowerOfFourO1(n int) bool {
+	//note 0x55555555 the only set bit is in an even position.
+	return n > 0 && (n&(n-1)) == 0 && (n&0x55555555) != 0
+}
